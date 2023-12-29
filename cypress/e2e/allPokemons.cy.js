6381b6b14d0fa264ac7c964de1e79746d4e264fb
Lenar Lima <lenar.toledo@gmail.com>
2023-12-29 17:24:11 -0400

    Add E2E tests to deployment Pipeline

---
+++ cypress/e2e/allPokemons.cy.js
@@ -0,0 +1,12 @@
+/* global cy */
+
+describe('Pokedex', function() {
+  it('frontpage can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').eq(0).click() 
+    cy.contains('pikachu')
+    cy.contains('Home').click()
+    cy.contains('bulbasaur')
+    // cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
+  })
+})
